68f83b2915af607e2833ecb728797dc641de8942
Jeremiah Gutherie <xjyg43q@Jeremiahs-MBP.lan>
2022-03-29 16:59:00 -0400

    First commit, should result in a bad dependency scan

---
+++ DependencyPlayground/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyPlayground.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get(string zipCode)
+    {
+        // try
+        // {
+        //     Console.WriteLine(string.Format(zipCode, "44256"));
+        // }
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine(ex.Message);
+        // }
+        
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
